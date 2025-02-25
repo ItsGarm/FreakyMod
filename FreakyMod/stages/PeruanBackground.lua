3ff5d5f5f9075dbd414580c26a4a48fc99f70815
ItsGarm <sepubenj42022@gmail.com>
2025-02-25 20:19:45 -0300

    Mod de mierda que ando haciendo para testear cosas
    :v

---
+++ FreakyMod/stages/PeruanBackground.lua
@@ -0,0 +1,21 @@
+function onCreate()
+
+	makeLuaSprite('PeruanBackground','PeruanBackground',-600,-300)
+	addLuaSprite('PeruanBackground',false)
+	setLuaSpriteScrollFactor('PeruanBackground', 0.9, 0.9);
+
+
+end
+
+
+function onBeatHit()-- for every beat
+	-- body
+end
+
+function onStepHit()-- for every step
+	-- body
+end
+
+function onUpdate()
+	-- body
+end
